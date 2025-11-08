9188715e9c1b305f033d3422166fd6c8f1d43445
Ray <raystormx3@yahoo.com>
2025-11-08 17:07:52 -0500

    refactor(LineSensor): Replace magic numbers with named constants for clarity

--- include/subsystems/line_sensor.hpp
+++ include/subsystems/line_sensor.hpp
@@ -25,9 +25,13 @@ class LineSensor : public Subsystem {
         friend class Subsystem;
         LineSensor();
 
+        static constexpr int NUM_SENSORS = 6;
+        static constexpr int SENSOR_MAX_VALUE = 3000;
+        static constexpr int SENSOR_READ_TIMEOUT_US = 10000;
+
         // Each value is in the range [0, 3000]
         // 0 means the sensor is on a very bright surface, and 3000 means the sensor is on a dark surface
-        int last_sensor_values_[6];
+        int last_sensor_values_[NUM_SENSORS];
 
         void update_sensor_values();
         bool is_line_detected();
--- src/subsystems/line_sensor.cpp
+++ src/subsystems/line_sensor.cpp
@@ -1,7 +1,7 @@
 #include "subsystems/line_sensor.hpp"
 
 LineSensor::LineSensor() {
-    for (int i = 0; i < 6; i++) {
+    for (int i = 0; i < NUM_SENSORS; i++) {
         last_sensor_values_[i] = 0;
     }
 }
@@ -12,9 +12,9 @@ void LineSensor::loop() {
 
 void LineSensor::log() {
     Serial.print("Line Sensor Values: ");
-    for (int i = 0; i < 6; ++i) {
+    for (int i = 0; i < NUM_SENSORS; ++i) {
         Serial.print(last_sensor_values_[i]);
-        if (i < 5) {
+        if (i < NUM_SENSORS - 1) {
             Serial.print(", ");
         }
     }
@@ -25,28 +25,28 @@ std::optional<double> LineSensor::get_line_position() {
     double weighted_sum = 0;
     double total_value = 0;
 
-    for (int i = 0; i < 6; ++i) {
+    for (int i = 0; i < NUM_SENSORS; ++i) {
         // Invert the value so that 0 means dark and 3000 means bright
-        double value = (3000 - last_sensor_values_[i]);
+        double value = (SENSOR_MAX_VALUE - last_sensor_values_[i]);
         // Center the weights around 0. Left sensors have negative weights, right sensors have positive weights
         // Since we have 6 sensors, the weights would be: -2.5, -1.5, -0.5, 0.5, 1.5, 2.5
-        double weight = i - (6 - 1) / 2.0;
+        double weight = i - (NUM_SENSORS - 1) / 2.0;
         weighted_sum += value * weight;
         total_value += value;
     }
 
     // Normalize to [-1, 1]
-    return weighted_sum / total_value / 2.5;
+    return weighted_sum / total_value / ((NUM_SENSORS - 1) / 2.0);
 }
 
 void LineSensor::update_sensor_values() {
-    for (int i = 0; i < 6; ++i) {
+    for (int i = 0; i < NUM_SENSORS; ++i) {
         pinMode(LINE_SENSOR_PINS[i], OUTPUT);
         digitalWrite(LINE_SENSOR_PINS[i], HIGH);
         delay(1);
         pinMode(LINE_SENSOR_PINS[i], INPUT);
         unsigned long long start = micros();
-        while (digitalRead(LINE_SENSOR_PINS[i]) && micros() - start < 10000) {}
+        while (digitalRead(LINE_SENSOR_PINS[i]) && micros() - start < SENSOR_READ_TIMEOUT_US) {}
         unsigned long long end = micros();
 
         int diff = end - start;
@@ -56,7 +56,7 @@ void LineSensor::update_sensor_values() {
 }
 
 bool LineSensor::is_line_detected() {
-    for (int i = 0; i < 6; ++i) {
+    for (int i = 0; i < NUM_SENSORS; ++i) {
         if (last_sensor_values_[i] < WHITE_THRESHOLD || last_sensor_values_[i] > BLACK_THRESHOLD) { 
             return true;
         }
