50ccd44b40edc40d8cc809f80f8b8cdf2f433e51
Ian Shi <ianshi1026@gmail.com>
2025-11-08 22:13:45 -0500

    get rid of line sensor, invert elevator

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,8 @@
 #include "pins.hpp"
 #include "subsystems/drivetrain.hpp"
 #include "subsystems/controller.hpp"
-#include "subsystems/line_sensor.hpp"
 #include "subsystems/elevator.hpp"
 #include "subsystems/ultrasonic_sensor.hpp"
-#include "commands.hpp"
 
 enum Mode {AUTONOMOUS, TELEOP};
 
@@ -13,7 +11,6 @@ Mode mode = TELEOP;
 
 Drivetrain* drivetrain;
 Controller* controller;
-LineSensor* line_sensor;
 Elevator* elevator;
 UltrasonicSensor* ultrasonic_sensor;
 
@@ -23,7 +20,6 @@ double auton_start_time;
 void setup() {
   drivetrain = &Drivetrain::get_instance();
   controller = &Controller::get_instance();
-  line_sensor = &LineSensor::get_instance();
   elevator = &Elevator::get_instance();
   ultrasonic_sensor = &UltrasonicSensor::get_instance();
   
@@ -32,7 +28,6 @@ void setup() {
 
 void update_subsystems() {
   controller->loop();
-  line_sensor->loop();
   drivetrain->loop();
   elevator->loop();
   ultrasonic_sensor->loop();
@@ -40,7 +35,6 @@ void update_subsystems() {
 
 void log_subsystems() {
   if (CONTROLLER_LOGGING_ENABLED) controller->log();
-  if (LINE_SENSOR_LOGGING_ENABLED) line_sensor->log();
   if (DRIVETRAIN_LOGGING_ENABLED) drivetrain->log();
   if (ELEVATOR_LOGGING_ENABLED) elevator->log();
   if (SONAR_LOGGING_ENABLED) ultrasonic_sensor->log();
@@ -59,11 +53,10 @@ void loop() {
   if (mode == TELEOP) {
     drivetrain->set_speed_based_on_joysticks(controller->get_left_y(), controller->get_right_x());
     
-    // Use shoulder buttons for the elevator
-    if (controller->is_RB_pressed()) {
-      elevator->set_speed(1.0); // Raise elevator
-    } else if (controller->is_LB_pressed()) {
-      elevator->set_speed(-1.0); // Lower elevator
+    if (controller->is_Y_pressed()) {
+      elevator->set_speed(0.5); // Raise elevator
+    } else if (controller->is_A_pressed()) {
+      elevator->set_speed(-0.5); // Lower elevator
     } else {
       elevator->set_speed(0.0); // Stop elevator
     }
@@ -73,8 +66,9 @@ void loop() {
     double elapsed = (millis() - auton_start_time) / 1000.0;
 
     if (elapsed < 3.0) { // Run autonomous for 3 seconds
-      commands::follow_line();
+      drivetrain->set_speed(0.5, 0.0);
     } else {
+      drivetrain->set_speed(0.0, 0.0);
       mode = TELEOP;
       auton_has_ran = true;
     }
